a528bca8d6d71c73e979f1da8094a70477c69b65
Rogério Terciotte <rterciotte@gmail.com>
2019-06-18 18:22:12 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -11,4 +11,13 @@ public class Utils {
 		
 	}
 	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
+	}
+		
 }
